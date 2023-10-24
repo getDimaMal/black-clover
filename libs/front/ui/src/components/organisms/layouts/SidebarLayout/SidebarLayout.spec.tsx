aae737f5442e5edc4c80afd1399d7036cbd977a7
“Malyshev” <getDimaMal@gmail.com>
2023-10-24 13:52:16 +0300

    FE Update SidebarLayout.spec.tsx

--- libs/front/ui/src/components/organisms/layouts/SidebarLayout/SidebarLayout.spec.tsx
+++ libs/front/ui/src/components/organisms/layouts/SidebarLayout/SidebarLayout.spec.tsx
@@ -4,13 +4,15 @@ import SidebarLayout from './SidebarLayout';
 
 describe('SidebarLayout', () => {
   it('should render', () => {
+    const label = 'label';
     const content = 'content';
     const { getByText } = customRender(
-      <SidebarLayout navigations={[]} onNavigate={jest.fn()}>
+      <SidebarLayout navigations={[[{ label, variant: 'item', path: '/' }]]} onNavigate={jest.fn()}>
         {content}
       </SidebarLayout>
     );
 
+    expect(getByText(label)).toBeInTheDocument();
     expect(getByText(content)).toBeInTheDocument();
   });
 });
