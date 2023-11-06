a60a2c588f79ef2576752f5d3111d6b2f3c4ff2b
“Malyshev” <getDimaMal@gmail.com>
2023-11-06 15:18:31 +0300

    FE Update SignUp tests

--- libs/front/services/src/components/auth/SignUp/SignUp.spec.tsx
+++ libs/front/services/src/components/auth/SignUp/SignUp.spec.tsx
@@ -1,41 +1,84 @@
 import React from 'react';
 import * as authHook from '@black-clover/front/api';
+import { CreateUserDto } from '@black-clover/shared/dto/users/create-user.dto';
 import { TokenUserDto } from '@black-clover/shared/dto/users/token-user.dto';
 
+import { fireEvent } from '../../../../jest.setup';
 import { customRender } from '../../../test-utils';
+import { FormContainerRenderProps } from '../../form/FormContainer';
 
-import SignUp from './SignUp';
+import SignUp, { SignUpProps } from './SignUp';
 
 type ResultType = ReturnType<typeof authHook.useSignUp>;
-
+const mockSignUp = jest.fn();
 const getResult = (result: Partial<ResultType> = {}): ResultType => ({
   user: null,
-  signUp: jest.fn(),
-  status: 'idle',
   error: null,
+  status: 'idle',
   isLoading: false,
+  signUp: mockSignUp,
   ...result,
 });
 jest.mock('@black-clover/front/api', () => ({
   useSignUp: jest.fn<ResultType, unknown[]>(() => getResult()),
 }));
 
+const getForm = (form: Partial<CreateUserDto> = {}): CreateUserDto => ({
+  email: 'mail@mail.com',
+  password: 'password123',
+  ...form,
+});
+const renderForm = (control: FormContainerRenderProps<CreateUserDto>['control'], form: CreateUserDto = getForm()) => {
+  control.current.setValue('email', form.email);
+  control.current.setValue('password', form.password);
+  return <div />;
+};
+
+const getProps = (props: Partial<SignUpProps> = {}): SignUpProps => ({
+  onSuccess: jest.fn(),
+  render: ({ control }) => renderForm(control),
+  ...props,
+});
+
 describe('SignUp', () => {
   it('should render default', () => {
     const test = 'test';
-    const onSuccess = jest.fn();
-    const { getByText } = customRender(<SignUp onSuccess={onSuccess} render={() => <div>{test}</div>} />);
+    const props = getProps({ render: () => <div>{test}</div> });
+    const { getByText } = customRender(<SignUp {...props} />);
 
-    expect(onSuccess).not.toHaveBeenCalled();
+    expect(props.onSuccess).not.toHaveBeenCalled();
     expect(getByText(test)).toBeInTheDocument();
   });
 
   it('should call onSuccess', () => {
     jest.spyOn(authHook, 'useSignUp').mockReturnValue(getResult({ user: {} as TokenUserDto, status: 'success' }));
 
-    const onSuccess = jest.fn();
-    customRender(<SignUp onSuccess={onSuccess} render={() => <div />} />);
+    const props = getProps();
+    customRender(<SignUp {...props} />);
 
-    expect(onSuccess).toHaveBeenCalledTimes(1);
+    expect(props.onSuccess).toHaveBeenCalledTimes(1);
+  });
+
+  describe('signUp', () => {
+    it('should call when form has NO errors', () => {
+      const { getByRole } = customRender(<SignUp {...getProps()} />);
+
+      fireEvent.submit(getByRole('form'));
+      expect(mockSignUp).toHaveBeenCalledTimes(1);
+      expect(mockSignUp).toHaveBeenCalledWith(getForm());
+    });
+
+    it.each<[string, CreateUserDto]>([
+      ['email is not valid', getForm({ email: 'Not Email' })],
+      ['password too short', getForm({ password: 'qwe123' })],
+      ['password has only numbers', getForm({ password: '123456' })],
+      ['password has only letters', getForm({ password: 'text password' })],
+    ])('should NOT call when: %s', (_, form) => {
+      const props = getProps({ render: ({ control }) => renderForm(control, form) });
+      const { getByRole } = customRender(<SignUp {...props} />);
+
+      fireEvent.submit(getByRole('form'));
+      expect(mockSignUp).not.toHaveBeenCalled();
+    });
   });
 });
